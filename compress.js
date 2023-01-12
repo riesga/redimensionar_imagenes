bd20d54cc72a667a1f837e2268e6374c8d76683b
Riesga <riesga@gmail.com>
2023-01-11 23:06:56 -0500

    Versión 1.0.2

---
+++ compress.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+const sourceFolder = './myfolder';
+
+fs.readdir(sourceFolder, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+        if (file.endsWith('.bak')) {
+            const filePath = `${sourceFolder}/${file}`;
+            const destination = `${filePath.slice(0,-4)}.zip`;
+            if(fs.existsSync(destination)) {
+                console.log(`${destination} already compressed, skipping`)
+                continue;
+            }
+            fs.stat(filePath, function(err, stats) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    var mtime = new Date(stats.mtime);
+                    var currentDate = new Date();
+                    var difference = currentDate - mtime;
+                    var daysDifference = difference / 1000 / 60 / 60 / 24;
+                    if (daysDifference > 2) {
+                        fs.unlink(filePath, (err) => {
+                            if (err) throw err;
+                            console.log(`${filePath} deleted`);
+                        });
+                    } else {
+                        const output = fs.createWriteStream(destination);
+                        const archive = archiver('zip', { zlib: { level: 9 } });
+                        output.on('close', function() {
+                            console.log(archive.pointer() + ' total bytes');
+                            console.log('archiver has been finalized and the output file descriptor has closed.');
+                        });
+
+                        output.on('end', function() {
+                            console.log('Data has been drained');
+                        });
+
+                        archive.on('warning', function(err) {
+                            if (err.code === 'ENOENT') {
+                                console.log("Archive warning: " + err);
+                            } else {
+                                throw err;
+                            }
+                        });
+
+                        archive.on('error', function(err) {
+                            throw err;
+                        });
+
+                        archive.pipe(output);
+                        archive.file(filePath, { name: file });
+                        archive.finalize();
+                    }
+                }
+            });
+        }
+    }
+});
--- resizeImage.js
+++ resizeImage.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const sharp = require('sharp');
 
-const folderPath = 'D:\\fotos';
-
+const folderPath = 'C:\\fotos';
+const height = 200; // Altura esperada
 
 fs.readdir(folderPath, (err, files) => {
   if (err) {
@@ -12,32 +12,44 @@ fs.readdir(folderPath, (err, files) => {
 
   files.forEach((file) => {
     if (file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.PNG') || file.endsWith('.JPG')) {
-      const tempFile = `${folderPath}/temp-${file}`;
-      
-      fs.copyFile(`${folderPath}/${file}`, tempFile, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          sharp(tempFile)
-            .resize({
-              fit: sharp.fit.inside,
-              withoutEnlargement: true,
-            }, 167)
-            .toFile(`${folderPath}/${file}`, (err, info) => {
-              if (err) {
+        const tempFile = `${folderPath}/temp-${file}`;
+        fs.copyFile(`${folderPath}/${file}`, tempFile, (err) => {
+            if (err) {
                 console.error(err);
-              } else {
-                fs.unlink(tempFile, (err) => {
-                  if (err) {
-                    console.error(err);
-                  } else {
-                    console.log(`Successfully resized ${file}`);
-                  }
-                });
-              }
-            });
-        }
-      });
+            } else {
+                sharp(tempFile).metadata()
+                  .then(function(metadata) {
+                      if (metadata.height >= height) {
+                        sharp(tempFile)
+                          .resize(null, height)
+                          .withMetadata()
+                          .toFile(tempFile, (err, info) => {
+                            if (err) {
+                              console.error(err);
+                            } else {
+                              fs.rename(tempFile,`${folderPath}/${file}`, (err) => {
+                                if (err) {
+                                  console.error(err);
+                                } else {
+                                  console.log(`Successfully resized ${file}`);
+                                }
+                              });
+                            }
+                          });
+                      } else {
+                        console.log(`${file} is already ${height}px height`);
+                        fs.unlink(tempFile, (err) => {
+                            if (err) {
+                                console.error(err);
+                            }
+                        });
+                      }
+                  })
+                  .catch(function(err) {
+                      console.log(err);
+                  });
+            }
+        });
     }
   });
 });
